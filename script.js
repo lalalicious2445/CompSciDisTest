d43c356fb58b9ff5d32d959090ae4239b7326bab
lalalicious2445 <larakaki@midpac.edu>
2026-01-13 10:51:21 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// This will pop up a window when the page loads
+alert("Hello World! Welcome to my project.");
+
+// This will log a message to the browser console (for you to see)
+console.log("Hello World from the code!");
